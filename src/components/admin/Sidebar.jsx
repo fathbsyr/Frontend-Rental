50439d306316b18422a8450af9f20747d2f3c24c
fathbsyr <fatihmubasyir0@gmail.com>
2024-12-11 08:34:14 +0700

    update template

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -29,72 +29,39 @@ function Sidebar() {
             </div>
 
             {/* Nav Item - Pages Collapse Menu */}
-            <li className="nav-item">
-                <a
-                    className="nav-link collapsed"
-                    href="#"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseTwo"
-                    aria-expanded="true"
-                    aria-controls="collapseTwo"
-                >
-                    <i className="fas fa-fw fa-cog"></i>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-cog"></i>
                     <span>Components</span>
                 </a>
-                <div
-                    id="collapseTwo"
-                    className="collapse"
-                    aria-labelledby="headingTwo"
-                    data-bs-parent="#accordionSidebar"
-                >
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <h6 className="collapse-header">Custom Components:</h6>
-                        <a className="collapse-item" href="buttons.html">
-                            Buttons
-                        </a>
-                        <a className="collapse-item" href="cards.html">
-                            Cards
-                        </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Custom Components:</h6>
+                        <a class="collapse-item" href="buttons.html">Buttons</a>
+                        <a class="collapse-item" href="cards.html">Cards</a>
                     </div>
                 </div>
             </li>
 
             {/* Nav Item - Utilities Collapse Menu */}
-            <li className="nav-item">
-                <a
-                    className="nav-link collapsed"
-                    href="#"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseUtilities"
-                    aria-expanded="true"
-                    aria-controls="collapseUtilities"
-                >
-                    <i className="fas fa-fw fa-wrench"></i>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-fw fa-wrench"></i>
                     <span>Utilities</span>
                 </a>
-                <div
-                    id="collapseUtilities"
-                    className="collapse"
-                    aria-labelledby="headingUtilities"
-                    data-bs-parent="#accordionSidebar"
-                >
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <h6 className="collapse-header">Custom Utilities:</h6>
-                        <a className="collapse-item" href="utilities-color.html">
-                            Colors
-                        </a>
-                        <a className="collapse-item" href="utilities-border.html">
-                            Borders
-                        </a>
-                        <a className="collapse-item" href="utilities-animation.html">
-                            Animations
-                        </a>
-                        <a className="collapse-item" href="utilities-other.html">
-                            Other
-                        </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Custom Utilities:</h6>
+                        <a class="collapse-item" href="utilities-color.html">Colors</a>
+                        <a class="collapse-item" href="utilities-border.html">Borders</a>
+                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                        <a class="collapse-item" href="utilities-other.html">Other</a>
                     </div>
                 </div>
-            </li>
+            </li>           
 
             {/* Divider */}
             <hr className="sidebar-divider" />
@@ -103,88 +70,48 @@ function Sidebar() {
             <div className="sidebar-heading">Addons</div>
 
             {/* Nav Item - Pages Collapse Menu */}
-            <li className="nav-item">
-                <a
-                    className="nav-link collapsed"
-                    href="#"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapsePages"
-                    aria-expanded="true"
-                    aria-controls="collapsePages"
-                >
-                    <i className="fas fa-fw fa-folder"></i>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                    aria-expanded="true" aria-controls="collapsePages">
+                    <i class="fas fa-fw fa-folder"></i>
                     <span>Pages</span>
                 </a>
-                <div
-                    id="collapsePages"
-                    className="collapse"
-                    aria-labelledby="headingPages"
-                    data-bs-parent="#accordionSidebar"
-                >
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <h6 className="collapse-header">Login Screens:</h6>
-                        <a className="collapse-item" href="login.html">
-                            Login
-                        </a>
-                        <a className="collapse-item" href="register.html">
-                            Register
-                        </a>
-                        <a className="collapse-item" href="forgot-password.html">
-                            Forgot Password
-                        </a>
-                        <div className="collapse-divider"></div>
-                        <h6 className="collapse-header">Other Pages:</h6>
-                        <a className="collapse-item" href="404.html">
-                            404 Page
-                        </a>
-                        <a className="collapse-item" href="blank.html">
-                            Blank Page
-                        </a>
+                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Login Screens:</h6>
+                        <a class="collapse-item" href="login.html">Login</a>
+                        <a class="collapse-item" href="register.html">Register</a>
+                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+                        <div class="collapse-divider"></div>
+                        <h6 class="collapse-header">Other Pages:</h6>
+                        <a class="collapse-item" href="404.html">404 Page</a>
+                        <a class="collapse-item" href="blank.html">Blank Page</a>
                     </div>
                 </div>
             </li>
 
             {/* Nav Item - Charts */}
-            <li className="nav-item">
-                <a className="nav-link" href="charts.html">
-                    <i className="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span>
-                </a>
+            <li class="nav-item">
+                <a class="nav-link" href="charts.html">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Charts</span></a>
             </li>
 
             {/* Nav Item - Tables */}
-            <li className="nav-item">
-                <a className="nav-link" href="tables.html">
-                    <i className="fas fa-fw fa-table"></i>
-                    <span>Tables</span>
-                </a>
-            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="tables.html">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Tables</span></a>
+            </li>   
 
             {/* Divider */}
-            <hr className="sidebar-divider d-none d-md-block" />
-
-            {/* Sidebar Toggler (Sidebar) */}
-            <div className="text-center d-none d-md-inline">
-                <button className="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
+            <hr class="sidebar-divider d-none d-md-block" />
 
             {/* Sidebar Message */}
-            <div className="sidebar-card d-none d-lg-flex">
-                <img
-                    className="sidebar-card-illustration mb-2"
-                    src="img/undraw_rocket.svg"
-                    alt="..."
-                />
-                <p className="text-center mb-2">
-                    <strong>SB Admin Pro</strong> is packed with premium features, components, and
-                    more!
-                </p>
-                <a
-                    className="btn btn-success btn-sm"
-                    href="https://startbootstrap.com/theme/sb-admin-pro"
-                >
-                    Upgrade to Pro!
-                </a>
+            <div class="sidebar-card d-none d-lg-flex">
+                <img class="sidebar-card-illustration mb-2" src="admin/img/undraw_rocket.svg" alt="..."></img>
+                <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
+                <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
             </div>
         </ul>
     );
